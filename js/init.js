a3fa6a9348baeb51af4dfbda1d5cf0097974772f
RobertoLL24 <152240065+RobertoLL24@users.noreply.github.com>
2023-12-04 20:42:40 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDihMhYjURax8-W32hvlWMyVuZCRfA0zAM",
+  authDomain: "primer-proyecto-llr.firebaseapp.com",
+  projectId: "primer-proyecto-llr",
+  storageBucket: "primer-proyecto-llr.appspot.com",
+  messagingSenderId: "499100981623",
+  appId: "1:499100981623:web:16f7f6b0499b99e1e82972",
+  measurementId: "G-RRZLBZZRR0"
+    });
